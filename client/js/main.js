1ca1e7298126ecc2e52b6d680d5220c0ecb5e93e
Vitalii Solianyk <vitalii.solianyk@trinetix.com>
2018-10-02 20:30:16 +0300

    Quiz render

--- client/js/main.js
+++ client/js/main.js
@@ -2,68 +2,64 @@
 
 (function() {
 
-    function getImage(callback) {
+    function getQuestions(callback) {
         var xhr = new XMLHttpRequest();
 
-        xhr.open('GET', 'https://dog.ceo/api/breeds/image/random');
-        
-        xhr.onload = function () {
-            callback(JSON.parse(xhr.response));
-        };
+        xhr.open('GET', '/data/questions.json');
+
         xhr.send();
-    }
-    //https://dog.ceo/dog-api/documentation/breed
-    function getUsers(callback) {
-        var xhr = new XMLHttpRequest();
 
-        xhr.open('GET', '/api/users');
-        
-        xhr.onload = function () {
+        xhr.onload = function() {
             callback(JSON.parse(xhr.response));
         };
-        xhr.send(JSON.stringify({test: true}));
     }
 
-    function saveUser(data, callback) {
-        var xhr = new XMLHttpRequest();
-
-        xhr.open('POST', '/api/users');
-        
-        xhr.onload = function () {
-            callback();
-        };
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.send(JSON.stringify(data));
+    function getAnswerTpl(question, index) {
+        var tpl = '';
+        switch(question.type) {
+            case 'input': 
+                tpl = '<input class="form-control" type="text" name="question' + index + '" id="question' + index + '">';
+                break;
+            case 'select': 
+                tpl = '<select class="form-control" name="question' + index + '" id="question' + index + '">';
+                for(var i = 0; i < question.options.length; i++) {
+                    tpl += '<option value="' + question.options[i] + '">' + question.options[i] + '</option>';
+                }
+                tpl += '</select>';
+                break;
+            case 'radio': 
+                for(var i = 0; i < question.options.length; i++) {
+                    tpl += 
+                    '<div class="radio">' +
+                        '<label>' +
+                            '<input type="radio" ' +
+                                'name="question'+ index +'answer'+i+'" ' +
+                                'id="question'+ index +'answer'+i+'" ' +
+                                'value="'+question.options[i]+'">' +
+                            question.options[i] +
+                        '</label>' +
+                    '</div>';
+                }
+                break;
+        }
+        return tpl;
     }
 
-    function renderUsers(list) {
-        var template = '';
+    function renderQuestions(questions) {
+        var tpl = '';
 
-        for(var i = 0; i < list.length; i++) {
-            template += '<li class="list-group-item">' + list[i].firstName +
-                ' ' + list[i].lastName + '</li>';
+        for(var i = 0; i < questions.length; i++) {
+            tpl += 
+            '<div class="form-group">' +
+                '<label>' + (i + 1) + '. ' + questions[i].question + '</label>' +
+                getAnswerTpl(questions[i], i) +
+            '</div>';
         }
 
-        document.getElementById('users-list').innerHTML = template;
+        document.getElementById('quiz').innerHTML = tpl;
     }
 
-    getUsers(renderUsers);
+    getQuestions(renderQuestions);
+    
 
-    getImage(function (data) {
-        document.getElementById('img').innerHTML =
-            '<img src="' + data.message +'" />';
-        console.log(data);
-    });
-
-    document.querySelector('form')
-        .addEventListener('submit', function(e) {
-            e.preventDefault();
-            var user = {
-                firstName: document.getElementById('firstName').value,
-                lastName: document.getElementById('lastName').value
-            };
-            saveUser(user, function () {
-                getUsers(renderUsers);
-            });
-        });
 })();
